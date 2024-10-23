07d4c83eb1a1ccf7ddcc604bca6d69fe3bd54355
gustavo <chimenes61@gmail.com>
2024-10-23 08:42:08 -0300

    route login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,17 @@
 import express, { Router } from 'express'
+import path from 'path'
 import users from './users'
 import token from './token'
-import login from './login'
 
 const router = Router()
 
 router.use(express.json())
 router.use(express.static(__dirname + '/../../public'))
   
-router.use('/login', login)
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/views/login.html'));
+});
+
 router.use('/users', users)
 router.use('/token', token)
 
